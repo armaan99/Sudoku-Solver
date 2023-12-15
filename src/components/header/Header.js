cfca5dfd53173c68b5c73c4ca66bd41177224aac
Armaan Agrawal <armaan@Armaans-MacBook-Air.local>
2023-12-15 10:41:07 +0530

    UI

---
+++ src/components/header/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./Header.css";
+
+export default function Header() {
+  return (
+    <div className="header">
+      <div className="title">Sudoku Solver</div>
+      <div className="sub-title">by - Armaan Agrawal</div>
+    </div>
+  );
+}
